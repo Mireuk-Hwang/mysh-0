6306215f74033f82edaec5731ecbb23f5e0ed95f
Mireuk Hwang <mireuk_hwang@naver.com>
2017-10-08 02:22:30 +0900

    Start Coding!

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char buffer[8096] = "";
+	char* parser[1024];
+	char* check;
+	char* token;
+	char key[] = {'\n', '\0'};
+	int i=0, flag=0;
+
+	*argc = 0;
+	
+
 }
