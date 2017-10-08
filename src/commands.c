163fe38fc75a746504cbacd72c0aff55b13807db
Mireuk Hwang <mireuk_hwang@naver.com>
2017-10-08 21:02:39 +0900

    I did my best

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,13 @@
 #include <string.h>
-
+#include <unisted.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  chdir(argv[1]);
   return 0;
 }
 
@@ -14,17 +15,19 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  char buf[2048];
+  getcwd(buf, 2048);
+  printf("%s\n", buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+ // TODO: Fill it!
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+ // TODO: Fill it!
   return 1;
 }
