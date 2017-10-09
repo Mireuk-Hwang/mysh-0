1dc079dc86bc2a9c596d31f0c6a1cbed3f759558
Mireuk Hwang <mireuk_hwang@naver.com>
2017-10-10 01:55:24 +0900

    Answer

--- src/commands.c
+++ src/commands.c
@@ -1,33 +1,57 @@
-#include <string.h>
-#include <unisted.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/limits.h>
+
 #include "commands.h"
 
-int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
+int do_cd(int argc, char** argv){
+	if(!validate_cd_argv(argc, argv))
+		return -1;
+	
+	if(chdir(argv[1] == -1)
+		return -1;
 
-  chdir(argv[1]);
-  return 0;
+	return 0;
 }
 
-int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+int do_pwd(int argc, char** argv){
+	if(!validate_pwd_argv(argc, argv))
+		return -1;
 
-  char buf[2048];
-  getcwd(buf, 2048);
-  printf("%s\n", buf);
+	char curdir[PATH_MAX];
 
-  return 0;
+	if(getcwd(curdir, PATH_MAX) == NULL)
+		return -1;
+
+	printf("%s\n", curdir);
+
+	return 0;
 }
 
-int validate_cd_argv(int argc, char** argv) {
- // TODO: Fill it!
-  return 1;
+int validate_cd_argv(int argc, char** argv){
+	if(argc != 2)
+		return 0;
+	if(strcmp(argv[0], "cd") != 0)
+		return 0;
+
+	struct stat buf;
+	stat(argv[1], &buf);
+
+	if(!S_ISDIR(buf.st_mode))
+		return 0;
+
+	return 1;
 }
 
-int validate_pwd_argv(int argc, char** argv) {
- // TODO: Fill it!
-  return 1;
+int validate_pwd_argv(int argc, char** argv){
+	if(argc != 1)
+		return 0;
+	if(strcmp(argv[0], "pwd") != 0)
+		return 0;
+
+	return 1;
 }
